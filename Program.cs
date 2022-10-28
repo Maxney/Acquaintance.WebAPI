e952a6f3d93d7718cf10d9793fe5f9970f468804
Maxney <Maxney2016@yandex.ru>
2022-10-28 19:41:13 +0300

    edit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Acquaintance.WebAPI.AppConfiguration.ApplicationExtensions;
+using Acquaintance.WebAPI.AppConfiguration.ServicesExtensions;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.AddSerilogConfiguration(); //Add serilog
+builder.Services.AddVersioningConfiguration(); //add api versioning
+builder.Services.AddControllers();
+builder.Services.AddSwaggerConfiguration(); //add swagger configuration
+
+var app = builder.Build();
+
+app.UseSerilogConfiguration(); 
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerConfiguration(); //use swagger
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
